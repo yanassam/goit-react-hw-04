d37fe704266b7083410ef98f9c0b2eb83251ee38
Yana <yanassam81@gmail.com>
2024-05-09 15:20:30 +0200

    fix

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+// const accessKey = "kqurjjRC0wq5u0imyFvKxZZzaRzkRW-XuMfm3B5N5q8";
+
+import { useEffect, useState } from "react";
+
+// import axios from 'axios';
+
+import "./App.css";
+
+// import {Loader, ErrorMessage, LoadMoreBtn, ImageModal, SearchBar,ImageGallery} from './components'
+import { ErrorMessage } from "./components/ErrorMessage/ErrorMessage.jsx";
+import { LoadMoreBtn } from "./components/LoadMoreBtn/LoadMoreBtn.jsx";
+import { Loader } from "./components/Loader/Loader.jsx";
+import { ImageModal } from "./components/ImageModal/ImageModal.jsx";
+import { SearchBar } from "./components/SearchBar/SearchBar.jsx";
+import { ImageGallery } from "./components/ImageGallery/ImageGallery.jsx";
+import { fetchSearchImages } from "./service/serviceAPI.js";
+
+function App() {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [hasMore, setHasMore] = useState(true);
+
+  const [selectedImage, setSelectedImage] = useState("");
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleSearch = async () => {
+      if (!searchTerm) return;
+      try {
+        setError(false);
+        setLoading(true);
+
+        const data = await fetchSearchImages(searchTerm, page);
+        console.log(data);
+        setImages((prevState) => [...prevState, ...data]);
+        setHasMore(data.length === 15);
+      } catch (error) {
+        console.error("Failed to fetch images:", error);
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    handleSearch();
+  }, [searchTerm, page]);
+
+  //функ- я добавления картинок по клик кнопк
+  const fetchMoreImages = () => {
+    setPage((prev) => prev + 1);
+  };
+  // модал
+  const handleImageClick = (imageUrl) => {
+    setSelectedImage(imageUrl);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+  const handleSubmit = (searchTerm) => {
+    setSearchTerm(searchTerm);
+    setPage(1);
+    setImages([]);
+  };
+  return (
+    <>
+      <h1>Enter what you want to receive?</h1>
+      <SearchBar onSearch={handleSubmit} />
+      <ImageGallery images={images} onImageClick={handleImageClick} />
+      {searchTerm && images.length === 0 && <h2>Nothing in search</h2>}
+      {images.length > 0 && hasMore && (
+        <LoadMoreBtn
+          onLoadMore={fetchMoreImages}
+          onImageClick={handleImageClick}
+        />
+      )}
+      {error && <ErrorMessage />}
+      {loading && <Loader />}
+      <ImageModal
+        isOpen={modalIsOpen}
+        onClose={closeModal}
+        imageSrc={selectedImage}
+      />
+    </>
+  );
+}
+
+export default App;
--- src/service/serviceAPI.js
+++ src/service/serviceAPI.js
@@ -2,13 +2,20 @@ import axios from "axios";
 const accessKey = "kqurjjRC0wq5u0imyFvKxZZzaRzkRW-XuMfm3B5N5q8";
 
 export const fetchSearchImages = async (searchPhoto, page = 1) => {
-  const response = await axios.get(`https://api.unsplash.com/search/photos`, {
-    params: {
-      query: searchPhoto,
-      page: page,
-      per_page: 15,
-      client_id: accessKey,
-    },
-  });
-  return response.data.results;
+  try {
+    const response = await axios.get(`https://api.unsplash.com/search/photos`, {
+      params: {
+        query: searchPhoto,
+        page: page,
+        per_page: 15,
+        client_id: accessKey,
+      },
+    });
+    //   return response.data.results;
+    // };
+    return response.data.results;
+  } catch (error) {
+    console.error("Error fetching images:", error);
+    return [];
+  }
 };
